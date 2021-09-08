5cc1fbf433fe8819e177315453e1521b094c3603
FIUME Roberta <roberta.fiume@hexagon.com>
2021-09-08 15:14:12 +0100

    added all cycle hooks

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { 
+  Component, 
+  OnInit, 
+  Input, 
+  ViewEncapsulation, 
+  OnChanges, 
+  SimpleChanges, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,16 +20,57 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated //none and native 
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements 
+  OnInit, 
+  OnChanges, 
+  DoCheck, 
+  AfterContentInit, 
+  AfterContentChecked, 
+  AfterViewInit, 
+  AfterViewChecked,
+  OnDestroy {
   @Input() element: {type: string, name: string, content: string}; //send data through Input decorator that make the property accessible to the parent 
+  @Input() name: string;
 
   @Input() passedToChild = "";
   constructor() { 
+    console.log('constructor called!')
+  }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log("CHANGES:", changes);
   }
 
   ngOnInit(): void {
- 
+    console.log('ngOnInit called!');
   }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!'); //it gets called whenever angular checks for any changes, 
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!'); //it's called once, it doesn't get initalised again. It checks the ng-content 
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called'); //it's called after each change detaction cycle
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called'); //it's called after the content has been checked 
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
+
+
+
 
 }
